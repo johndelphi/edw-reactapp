3982cfcb6bed1b9ddc47b1d467a0ced601687765
John Michael <jmichael@coverys.com>
2023-04-05 15:16:01 -0500

    Class component

---
+++ src/Components/WelcomeMessageClassComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class WelcomeMessageClassComponent extends Component {
+    render() {
+        return (
+
+            <h1> Welcome to our insurance policy from class componet manager! </h1>
+        );
+    }
+}
+
+export default WelcomeMessageClassComponent;
