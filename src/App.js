b7c00526c30f02decc56a6591ee7bafef277c059
John Michael <jmichael@coverys.com>
2023-05-08 19:16:03 -0500

    added header for logout and routing demo

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import AuthenticateComponent from "./Components/AuthenticateComponent";
 import CoveragesComponent from "./Components/CoveragesComponent";
 
 function App() {
-    const [token, setToken] = useState(getStoredToken());
 
-    function getStoredToken() {
-        const storedToken = localStorage.getItem("token");
-        return storedToken || "";
+  const [token, setToken] = useState(getStoredToken());
+
+  function getStoredToken() {
+    const storedToken = localStorage.getItem("token");
+    return storedToken || "";
+  }
+
+  function renderContent() {
+    if (token) {
+      return <CoveragesComponent token={token} />;
+    } else {
+      return <AuthenticateComponent onToken={setToken} />;
     }
+  }
 
-    function renderContent() {
-        if (token) {
-            return <CoveragesComponent token={token} />;
-        } else {
-            return <AuthenticateComponent onToken={setToken} />;
-        }
-    }
-
-    return <div className="App">{renderContent()}</div>;
+  return <div className="App">{renderContent()}</div>;
 }
 
 export default App;
